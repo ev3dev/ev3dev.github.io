006d8e83af8d645eb0e866c19bd04afc3355258d
WasabiFan <wasabifan@outlook.com>
2014-08-03 18:49:52 -0700

    Fix user card link styles

--- javascripts/cards.js
+++ javascripts/cards.js
@@ -3,11 +3,14 @@
         var $cardDiv = $(element);
         $.ajax('https://api.github.com/users/' + $cardDiv.data('card-user')).done(function (userData) {
             $cardDiv.addClass('user-card');
+            $cardDiv.append('<a href="' + userData.html_url + '"></a>');
+            $cardDiv = $cardDiv.children(':last-child')
+
             $cardDiv.append('<div class="user-card-avatar"><img src="' + userData.avatar_url + '"/></div>');
             $cardDiv.append('<div class="user-card-text"></div>');
             var $textDiv = $cardDiv.children(':last-child');
             $textDiv.append('<div class="user-card-name">' + (userData.name || userData.login) + '</div>');
-            $textDiv.append('<div class="user-card-login"><a href="' + userData.html_url + '">@' + userData.login + '</a></div>');
+            $textDiv.append('<div class="user-card-login">@' + userData.login + '</div>');
         });        
     });
 });
