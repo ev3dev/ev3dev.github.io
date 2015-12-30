261ac172b3a6a1aa927590ca34c73aeb00ed99cd
WasabiFan <wasabifan@outlook.com>
2015-12-30 14:20:24 -0500

    Add initial version of Travis config

---
+++ cibuild.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+echo "Searching for BOMs -------------------------------"
+
+FOUND_BOM=false
+for filename in ./**/*.*; do
+    # Make sure that the file is UTF-8 so we don't search binary files or other encodings
+    CURRENT_FILE_ENCODING="`file --mime-encoding --brief "$filename"`"
+    if [ "$CURRENT_FILE_ENCODING" == "utf-8" ] && [ "`head -c 3 -- "$filename"`" == $'\xef\xbb\xbf' ]
+    then
+        # Make note of all the files that failed so we can see it in the Travis log
+        FOUND_BOM=true
+        echo "Found BOM in file $filename!"
+    fi
+done
+
+if [ $FOUND_BOM == true ]
+then
+    # We still want to run the other validation checks even if we found BOMs
+    echo "Checks failed! Jekyll can't handle BOMs. See above for list of problematic files."
+fi
+
+echo "Building site ------------------------------------"
+bundle exec jekyll build --trace
+
+echo "Validating HTML ----------------------------------"
+bundle exec htmlproof ./_site --disable-external
+
+# If the site build succeeded but we found BOMs, we want to fail the build
+if [ $FOUND_BOM == true ]
+then
+    exit 1
+fi
