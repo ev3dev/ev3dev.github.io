e6bed0574091fa65d331fb612acb6086d448a54b
WasabiFan <wasabifan@outlook.com>
2016-06-14 17:51:41 -0700

    Make user cards functional when JS is disabled

--- javascripts/cards.js
+++ javascripts/cards.js
@@ -5,31 +5,29 @@ $(document).ready(function () {
     $('div[data-card-user]').each(function (i, element) {
         var $cardDiv = $(element);
         getApiValue('https://api.github.com/users/' + $cardDiv.data('card-user'), userCardCacheTimeMillis, function (userData, error) {
-            $cardDiv.addClass('user-card');
-
-            if (error) {
-                console.error("User cards not available! Using static text card instead.");
-                $cardDiv.append('<div class="user-card-name">@' + $cardDiv.data('card-user')  + '</div>');
+            if (error || !userData) {
+                console.error("User card data not available! Using static text card instead.");
+                return;
             }
-            else {
-                $cardDiv = $('<a href="' + userData.html_url + '"></a>').appendTo($cardDiv);
-                
-                var $avatarDiv = $('<div class="user-card-avatar"><img src="' + userData.avatar_url + '"/></div>').appendTo($cardDiv);
-                var $textDiv = $('<div class="user-card-text"></div>').appendTo($cardDiv);
+            
+            $cardDiv.empty();
+            $cardDiv = $('<a href="' + userData.html_url + '"></a>').appendTo($cardDiv);
+            
+            var $avatarDiv = $('<div class="user-card-avatar"><img src="' + userData.avatar_url + '"/></div>').appendTo($cardDiv);
+            var $textDiv = $('<div class="user-card-text"></div>').appendTo($cardDiv);
 
-                $textDiv.append('<div class="user-card-name">' + (userData.name || userData.login) + '</div>');
-                $textDiv.append('<div class="user-card-login">@' + userData.login + '</div>');
+            $textDiv.append('<div class="user-card-name">' + (userData.name || userData.login) + '</div>');
+            $textDiv.append('<div class="user-card-login">@' + userData.login + '</div>');
 
-                // Give the text a margin to make sure it does not overlap the avatar
-                $avatarDiv.resize(function () {
-                    $textDiv.css({
-                        width: 'auto',
-                        "margin-left": $avatarDiv.height()
-                    });
+            // Give the text a margin to make sure it does not overlap the avatar
+            $avatarDiv.resize(function () {
+                $textDiv.css({
+                    width: 'auto',
+                    "margin-left": $avatarDiv.height()
                 });
+            });
 
-                $avatarDiv.resize();
-            }
+            $avatarDiv.resize();
         });
     });
 });
